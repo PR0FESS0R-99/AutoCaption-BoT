4f097ece9dbafd62f739e70316b12715eb2e5965
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-12 22:43:43 +0530

    🔻

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K & Pr0fess0r-99
+
 import os
 from config import Config
 
@@ -6,8 +10,31 @@ from pyrogram import Client, PAC_cs
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 
-CAPTION=Config.CAPTION
+FIXED_CAPTION=Config.CAPTION
 
 @PAC_cs.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
-    await message.edit(f"{message.file_name}\n\nCAPTION")
+    kopp, _ = get_file_id(message)
+    await message.edit(f"{kopp.file_name}\n\{FIXED_CAPTION}")
+
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                return obj, obj.file_id

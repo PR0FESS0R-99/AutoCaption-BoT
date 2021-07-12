41923ba1e59f08d8267dc8a71856b9f7fa33ff23
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-12 22:05:31 +0530

    🔻

--- mt_privateautocaption/commands.py
+++ mt_privateautocaption/commands.py
@@ -1,1 +1,7 @@
+# (C) PR0FESSOR-99
 
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+  await message.reply("Hy")

06be092f1f2a4bfb69a181fdb644f267f4b8b91b
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-12 23:00:05 +0530

    Update commands.py

--- mt_privateautocaption/commands.py
+++ mt_privateautocaption/commands.py
@@ -1,7 +1,62 @@
 # (C) PR0FESSOR-99
 
-from pyrogram import Client, filters
+from pyrogram import filters
+from pyrogram import Client, PAC_mt
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-@Client.on_message(filters.command("start") & filters.private)
-async def start(client, message):
-  await message.reply("Hy")
+@PAC_mt.on_message(filters.private & filters.command("start"))
+async def start(PAC_mt, update):
+    text = f"""<b> 👋Hello {update.from_user.mention}</b>"""
+    reply_markup =  MT_START
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+  )
+
+@Mo_Tech_YT.on_message(filters.command(["help"]) & filters.private, group=1)
+async def help(PAC_mt, update):
+    buttons = [[
+        InlineKeyboardButton("📃AutoFilter📃", url="t.me/mo_tech_yt")
+        ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=f"""<b> 👋Hello {update.from_user.mention}</b>"""
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+
+MT_START = InlineKeyboardMarkup(
+     [[
+        InlineKeyboardButton("🗣️Group", url=f"t.me/mo_tech_group"),
+        InlineKeyboardButton("📑Bot List", url=f"t.me/mo_tech_yt")
+     ]]
+   )
+
+@PAC_mt.on_callback_query(filters.regex(r"^(heroku|close)$"), group=2)
+async def callback_data(PAC_mt, update: CallbackQuery):
+
+    query_data = update.data
+
+    if query_data == "heroku":
+        buttons = [[
+            InlineKeyboardButton("🔻Click Here🔻", url="https://dashboard.heroku.com/")
+            ]]
+    
+        reply_markup = InlineKeyboardMarkup(buttons)
+
+        await update.message.edit_text(
+            """Hy"",
+            reply_markup=reply_markup,
+            parse_mode="html"
+        )
+
+
+    elif query_data == "close":
+        await update.message.delete()
+
+

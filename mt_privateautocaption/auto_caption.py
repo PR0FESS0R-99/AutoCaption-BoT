95f394da5d4797c48e40f1a4e78a0d34cbfe21d8
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-12 20:02:42 +0530

    🔻

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -1,1 +1,13 @@
+import os
+from config import Config
 
+from pyrogram import filters
+from pyrogram import Client, PAC_cs
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import FloodWait
+
+CAPTION=Config.CAPTION
+
+@PAC_cs.on_message(filters.media & filters.channel)
+async def caption(client, message: Message):
+    await message.edit(f"{message.file_name}\n\nCAPTION")

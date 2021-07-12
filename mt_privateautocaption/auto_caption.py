768e17d880b55eef333d99d3140f46d153ef0307
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-07-12 22:23:51 +0530

    add file name

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 from config import Config
 
@@ -10,4 +26,27 @@ CAPTION=Config.CAPTION
 
 @PAC_cs.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
-    await message.edit(f"{message.file_name}\n\nCAPTION")
+    kopp, _ = get_file_id(message)
+    await message.edit(f"{kopp.file_name}\n\nCAPTION")
+
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                return obj, obj.file_id

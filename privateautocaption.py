8df12b8a8b19727027f748a7862e302424efa8df
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-13 13:32:03 +0530

    ❣️

--- privateautocaption.py
+++ privateautocaption.py
@@ -7,15 +7,15 @@ from config import Config
 
 
 if __name__ == "__main__" :
-    plugins = dict(
-        root="plugins"
+    mt_privateautocaption = dict(
+        root="mt_privateautocaption"
     )
     MoTech = pyrogram.Client(
         "CaptionBot",
         bot_token=Config.MT_BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins,
+        mt_privateautocaption=mt_privateautocaption,
         workers=300
     )
     MoTech.run()

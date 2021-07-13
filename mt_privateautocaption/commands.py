e972bbab0502091a51ffb4e0a6e1b059906cb1fe
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-13 12:50:45 +0530

    ❣️

--- mt_privateautocaption/commands.py
+++ mt_privateautocaption/commands.py
@@ -1,62 +1,48 @@
+
+
 # (C) PR0FESSOR-99
 
-from pyrogram import filters
-from pyrogram import Client, PAC_mt
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-@PAC_mt.on_message(filters.private & filters.command("start"))
+BOT_USERNAME = "demo2022Bot"
+
+@Client.on_message(filters.private & filters.command("start"))
 async def start(client, update):
-    text = f"""<b> 👋Hello {update.from_user.mention}</b>"""
-    reply_markup =  MT_START
+    text = f"""<b> 👋Hello {update.from_user.mention}\n\nI am an AutoCaption bot\n\nAll you have to do is add me to your channel and I will show you my power\n\nFor more info check help Button</b>"""
+    reply_markup =  InlineKeyboardMarkup(
+                                         [[
+        InlineKeyboardButton("help↗️", callback_data="heroku"),
+        InlineKeyboardButton("🗣️Group", url="t.me/mo_tech_Group"),
+        InlineKeyboardButton("Channel📢", url="t.me/mo_tech_yt")
+        ]]
+    )
     await update.reply_text(
         text=text,
         disable_web_page_preview=True,
         reply_markup=reply_markup
   )
 
-@PAC_mt.on_message(filters.command(["help"]) & filters.private, group=1)
-async def help(client, update):
-    buttons = [[
-        InlineKeyboardButton("📃AutoFilter📃", url="t.me/mo_tech_yt")
-        ]]
-    
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=f"""<b> 👋Hello {update.from_user.mention}</b>"""
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
-
-MT_START = InlineKeyboardMarkup(
-     [[
-        InlineKeyboardButton("🗣️Group", url=f"t.me/mo_tech_group"),
-        InlineKeyboardButton("📑Bot List", url=f"t.me/mo_tech_yt")
-     ]]
-   )
-
-@PAC_mt.on_callback_query(filters.regex(r"^(heroku|close)$"), group=2)
+@Client.on_callback_query(filters.regex(r"^(heroku|motech)$"))
 async def callback_data(client, update: CallbackQuery):
 
     query_data = update.data
 
     if query_data == "heroku":
         buttons = [[
-            InlineKeyboardButton("🔻Click Here🔻", url="https://dashboard.heroku.com/")
+            InlineKeyboardButton("🖥️ Tutorial Video 🖥️", url="https://dashboard.heroku.com/")
+            ],[
+            InlineKeyboardButton("🏠Home", url=f"https://t.me/{BOT_USERNAME}?start=start"),
+            InlineKeyboardButton("❌️Close", callback_data="motech")
             ]]
     
         reply_markup = InlineKeyboardMarkup(buttons)
 
         await update.message.edit_text(
-            """Hy"",
+            """<b>AutoCaption Bot\n\nTake a look at the end of the video\nIt has to say\n\nYoutube Tutorial Video\n\nHeroku 👉 https://dashboard.heroku.com/\n\n© @Mo_Tech_YT</b>""",
             reply_markup=reply_markup,
             parse_mode="html"
         )
 
-
-    elif query_data == "close":
+    elif query_data == "motech":
         await update.message.delete()
-
-

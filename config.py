9153192544f23518a6ff5b841f6ffb930177a551
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-13 20:07:43 +0530

    ❣️

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 
 class Config(object):
     MT_BOT_TOKEN = os.environ.get("MT_BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
     CAPTION = os.environ.get("CAPTION", "@Mo_Tech_YT @Mo_Tech_Group")
     BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")

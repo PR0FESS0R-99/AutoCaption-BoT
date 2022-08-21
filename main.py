c641b26cec3b622fcd93ea833252bab961a4ab3a
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-08-21 17:02:06 +0530

    ..

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pyrogram, os, asyncio
 
-try: api_id = int(os.environ.get("api_id", None))
-except Exception as api_id: print(f"⚠️ Api ID Invalid {api_id}")
+try: app_id = int(os.environ.get("app_id", None))
+except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
 try: api_hash = os.environ.get("api_hash", None)
 except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
 try: bot_token = os.environ.get("bot_token", None)
@@ -10,7 +10,7 @@ try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
 AutoCaptionBotV1 = pyrogram.Client(
-   name="AutoCaptionBotV1", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+   name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
 
 start_message = """
 <b>👋Hello {}</b>
